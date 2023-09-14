6af5b326b092a88c07eb71fff6c9815aa2245c71
GyLevent <gyurkol@kkszki.hu>
2023-09-14 09:10:52 +0200

    Change

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static ProductApi.DTOS;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<ProductDTO> products = new()
+        {
+            new ProductDTO(Guid.NewGuid(),"Termék1",2500,DateTimeOffset.UtcNow,DateTimeOffset.UtcNow),
+            new ProductDTO(Guid.NewGuid(),"Termék2",2400,DateTimeOffset.UtcNow,DateTimeOffset.UtcNow),
+            new ProductDTO(Guid.NewGuid(),"Termék3",2300,DateTimeOffset.UtcNow,DateTimeOffset.UtcNow)
+        };
+    }
+}
